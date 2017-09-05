53795e3a54db6e9d6a6111338fad30992d1e67ba
Choyoonjeong <31638923+Choyoonjeong@users.noreply.github.com>
2017-09-05 12:01:09 +0900

    1st Assignment

---
+++ firstassignment.java
@@ -0,0 +1,10 @@
+
+public class firstassignment {
+	public static void main(String args[]) {
+		System.out.println("Your student id number: 2016301073");
+		System.out.println("Your name: Á¶À±Á¤");
+		System.out.println("Your mobile phone number: 010-3155-1888");
+		System.out.println("Your GitHub address: GitHub.com/Choyoonjeong");
+		
+	}
+}
